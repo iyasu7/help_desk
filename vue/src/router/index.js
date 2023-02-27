38347819b86043d5621a6ee7d02d0e3ca09383f9
iyasu7 <107022433+iyasu7@users.noreply.github.com>
2023-02-27 10:56:13 +0300

    try

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,57 +1,57 @@
-
 import { createRouter, createWebHashHistory } from "vue-router";
-import Login from '../views/Login.vue'
-import Dashboard from '../views/Dashboard.vue'
-import DefaultLayout from '../components/DefaultLayout.vue'
-import AuthLayout from '../components/AuthLayout.vue'
-import Register from '../views/Register.vue'
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import Register from "../views/Register.vue";
 import store from "../store";
+import router from "vue-router";
 const routes = [
     {
-        path:'/',
+        path: "/",
         component: DefaultLayout,
-        meta:{requiresAuth:true},
-        children:[
-            {path:'/dashboard', name:'Dashboard', component:Dashboard}
-        ]
+        meta: { requiresAuth: true },
+        children: [
+            { path: "/dashboard", name: "Dashboard", component: Dashboard },
+        ],
     },
     {
-        path:'/auth',
-        redirect:'/login',
-        name:'Auth',
+        path: "/auth",
+        redirect: "/login",
+        name: "Auth",
         component: AuthLayout,
         // to redirect if the user doesnt have an authorization
-        meta:{ isGuest:true },
-        children:[{ 
-                path:'/register',
-                name:'Register',
-                component: Register
-                },
-                {
-                path:'/login',
-                name:'Login',
-                component: Login
-                },
-    ]
+        meta: { isGuest: true },
+        children: [
+            {
+                path: "/register",
+                name: "Register",
+                component: Register,
+            },
+            {
+                path: "/login",
+                name: "Login",
+                component: Login,
+            },
+        ],
     },
-]
+];
 const router = createRouter({
     history: createWebHashHistory(),
-    routes
-})
+    routes,
+});
 router.beforeEach((to, from, next) => {
-    if (sessionStorage.getItem('TOKEN')) {
-        store.state.user.token = sessionStorage.getItem('TOKEN');
+    if (sessionStorage.getItem("TOKEN")) {
+        store.state.user.token = sessionStorage.getItem("TOKEN");
     }
     //if user is NOT authorized
-    if(to.meta.requiresAuth && !store.state.user.token){
-        next({name:'Login'})
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "Login" });
         // if user is already authorized
-    }else if(store.state.user.token && (to.meta.isGuest)) {
-        next({name: 'Dashboard'})
-    }
-    else{
+    } else if (store.state.user.token && to.meta.isGuest) {
+        next({ name: "Dashboard" });
+    } else {
         next();
     }
-})
+});
 export default router;
